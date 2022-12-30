4da2a7e3d2911e8154a29088c46d9b1b6bf70623
geky <geky@esupplychain.com>
2022-12-30 18:59:33 +0800

    Docker nginx nuxt

--- docker/ipass/platform-web/nuxt/start.sh
+++ docker/ipass/platform-web/nuxt/start.sh
@@ -2,5 +2,6 @@
 cd /usr/local/nginx/sbin
 ./nginx
 cd /platform-web/dist
-# pm2-runtime pm2为解决Docker 前台运行
+# pm2专为在后台发送或运行应用程序的正常使用而设计.
+# pm2-runtime 是为Docker容器设计的，它将应用程序置于前台，从而使容器保持运行状态，
 pm2-runtime start npm --name "platform-web" -- run start
